1df0d241ffde0076dc9b7cda542aadf73aabc0e1
romerodiego <diegoacuario11@gmail.com>
2014-10-24 20:18:11 -0500

    Versión inicial de mi web

---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="keywords" content="diegoacuario, Ingenio de ingenieros" />
+        <title>Inicio con Diego</title>
+        <meta name="format-detection" content="telephone=no" />
+        <link rel="icon" href="images/ico.png">
+
+        <link rel="stylesheet" href="css/style.css">
+        <link href='http://fonts.googleapis.com/css?family=Economica:700' rel='stylesheet' type='text/css'>
+        <script src="js/jquery.js"></script>
+        <script src="js/jquery-migrate-1.1.1.js"></script>
+        <script src="js/script.js"></script>
+        <script src="js/jquery.ui.totop.js"></script>
+        <script src="js/superfish.js"></script>
+        <script src="js/jquery.equalheights.js"></script>
+        <script src="js/jquery.mobilemenu.js"></script>
+        <script src="js/jquery.easing.1.3.js"></script>
+        <script>
+            $(document).ready(function () {
+                $().UItoTop({easingType: 'easeOutQuart'});
+            })
+        </script>
+        <!--[if lt IE 8]>
+        <div style=' clear: both; text-align:center; position: relative;'>
+                <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+                        <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+                </a>
+        </div>
+        <![endif]-->
+        <!--[if lt IE 9]>
+        <script src="js/html5shiv.js"></script>
+        <link rel="stylesheet" media="screen" href="css/ie.css">
+        <![endif]-->
+    </head>
+    <body class="page1" id="top">
+        <!--==============================header=================================-->
+        <div class="main">
+            <header>
+                <div class="clear"></div>
+                <div class="container_12">
+                    <div class="grid_12">
+                        <h1>
+                            <a href="index.php">
+                                <img src="images/logo.png" alt="Diego Romero Armijos, Ingeniero en Sistemas">
+                            </a>
+                        </h1>
+                    </div>
+                </div>
+            </header>
+        </div>
+        <div class="menu_block">
+            <div class="container_12">
+                <div class="grid_12">
+                    <nav class="horizontal-nav full-width horizontalNav-notprocessed">
+                        <ul class="sf-menu">
+                            <li class="current"><a href="index.php">Inicio</a></li>
+                            <li><a href="index-1.html">Acerca de él</a></li>
+                            <li><a href="index-2.html">Fotos</a></li>
+                            <li><a href="index-3.html">Videos</a></li>
+                            <li><a href="index-4.html">Contactos</a></li>
+                        </ul>
+                    </nav>
+                    <div class="clear"></div>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+        <div class="main">
+            <!--==============================Content=================================-->
+            <div class="content"><div class="ic">Más en mi facebook - Septiembre 26, 2014!</div>
+                <div class="container_12">
+                    <div class="grid_6">
+                        <div class="p1_box left cl1">
+                            <div class="type"></div>
+                            <img src="images/page1_img1.jpg" alt="">
+                            <a href="index-2.html" class="bot">Familia y más <span>6<br>fotos más</span></a>
+                        </div>
+                        <div class="p1_box left cl2">
+                            <div class="type"></div>
+                            <div class="gray_block">
+                                Más fotos en <span class="col1"><a href="http://facebook.com/diegoacuario/" target="_blank" rel="nofollow">mi facebook</a></span> agregamé y ponte en contacto.<br>
+                                Más información en <span class="col1"><a href="http://diegoacuario.bloghspot.com/" rel="nofollow">mi blog</a></span>
+                            </div>
+                            <a href="https://www.facebook.com/photo.php?fbid=10200748609916379&set=a.2042066470057.75990.1796204536&type=1" target="_blank"class="bot">Comentarios<span>más<br>comentarios</span></a>
+                        </div>
+                        <div class="p1_box left cl1 pos1">
+                            <div class="type"></div>
+                            <img src="images/page1_img4.jpg" alt="">
+                            <a href="index-2.html" class="bot">Ayleen, mija con su peluchito<span>6<br>fotos más</span></a>
+                        </div>
+                        <div class="p1_box left cl3 pos1">
+                            <div class="type"></div>
+                            <img src="images/page1_img6.jpg" alt="">
+                            <a href="index-3.html" class="bot">Coexisting with Nature<span><strong class="fa-angle-right fa"></strong></span></a>
+                        </div>
+                    </div>
+                    <div class="grid_6">
+                        <div class="p1_box right cl1">
+                            <div class="type"></div>
+                            <img src="images/page1_img2.jpg" alt="">
+                            <a href="index-2.html" class="bot">Ayleen mi preciosa hija <span>6<br>fotos más</span></a>
+                        </div>
+                        <div class="p1_box right cl3">
+                            <div class="type"></div>
+                            <img src="images/page1_img3.jpg" alt="">
+                            <a href="index-3.html" class="bot">Night Sky Video<span><strong class=" fa-angle-right fa"></strong></span></a>
+                        </div>
+                        <div class="p1_box right cl4">
+                            <div class="type"></div>
+                            <a href="#" class="bot">Inspirational Resources</a>
+                        </div>
+                        <div class="p1_box right cl5">
+                            <div class="type"></div>
+                            <blockquote>To me, photography is an art of observation. It’s about finding something interesting in an ordinary place... I’ve found it has little to do with the things you see and everything to do with the way you see them. </blockquote>
+                            <a href="#" class="bot">Elliott Erwitt <span>8 <br>comments</span></a>
+                        </div>
+                        <div class="p1_box right cl2">
+                            <div class="type"></div>
+                            <img src="images/page1_img7.jpg" alt="">
+                            <a href="#" class="bot">My new project<span><strong class=" fa-angle-right fa"></strong></span></a>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                    <div class="grid_12">
+                        <a href="#" class="round"> Older Posts</a>
+                    </div>
+                </div>
+            </div>
+            <!--==============================footer=================================-->
+            <footer>
+                <div class="hor bg3"></div>
+                <div class="container_12">
+                    <div class="grid_12">
+                        <div class="socials">
+                            <a href="http://www.facebook.com/diegoacuario" target="_blank"></a>
+                            <a href="#"></a>
+                            <a href="#"></a>
+                            <div class="clear"></div>
+                        </div>
+                        <div class="copy">
+                            &copy; <span id="copyright-year"></span> | <a href="#">Política de privacidad</a> <br>
+                            Página diseñada por <a href="http://www.facebook.com/diegoacuario" rel="nofollow">Diego Romero Armijos</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </body>
+</html>
